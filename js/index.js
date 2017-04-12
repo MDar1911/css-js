0950f14b5375b7cba92ff6089b0e9d97371c216e
xxccmd <a564268706@outlook.com>
2017-04-12 18:18:49 +0800

    css+js

---
+++ js/index.js
@@ -0,0 +1,26 @@
+﻿	
+	 function check(){
+    	if(check1()&&check2()){
+			return true;
+		}else{
+			return false;
+		}
+    }
+	function check1() {
+    	var doc1=document.form.text1.value;
+        if(doc1!="徐志达"){
+    	alert("请输入：徐志达");
+		return false;	
+    	}else{
+    		return true;
+    	}
+    }
+	function check2() {
+    	var doc2=document.form.text2.value;
+    	if(doc2!="123"){
+    	alert("请输入ID：123");
+		return false;	
+    	}else{
+    		return true;
+    	}
+    }
